bf808e16cd8a78a9f3f2b0004d049b844bbea917
HermanMusyoki <salmantso14@gmail.com>
2025-03-28 11:43:51 +0300

    rendering data from the database in a view

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse
 
+from articles.models import Article
+
+#from database
+article_obj = Article.objects.get(id=1)
+
+article_title = article_obj.title
+article_content = article_obj.content
+
+
 name = "Solomon"
 HTML_RESPONSE = f"""
-<h1>Hello {name}</h1>
+<h1> {article_obj.id} article_title</h1>
+<h4>article_content</h4>
 """
 
 def home(request):
