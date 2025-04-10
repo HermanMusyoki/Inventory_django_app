9708343f612164d680c5ca7f13bdaa789594e946
HermanMusyoki <salmantso14@gmail.com>
2025-04-10 17:01:58 +0300

    added new models

--- articles/models.py
+++ articles/models.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Article(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=120)
     content = models.TextField()
+    timestamb = models.DateTimeField(auto_now_add= True)
+    updated = models.DateTimeField(auto_now= True)
