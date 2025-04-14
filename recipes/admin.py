05e19ba3f61458dcc7c31e2b9d2c708ba2ed5c58
HermanMusyoki <salmantso14@gmail.com>
2025-04-14 15:55:41 +0300

    data models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.contrib import admin
 
 from .models import Recipe, RecipeIngredient
 
-admin.site.register(Recipe)
-
+User = get_user_model()
 admin.site.register(RecipeIngredient)
+
+class UserInline(admin.TabularInline):
+    model = User
+
+class RecipeIngredientInline(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+
+class RecipeAdmin( admin.ModelAdmin):
+    list_display = ['name', 'user']
+    readonly_fields = ['timestamp', 'updated']
+    raw_id_fields = ['user']
+
+
+admin.site.register(Recipe)
